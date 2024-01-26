aa847eb69f9ebd7ba46a3b4b1ae0f284480e726b
Subhanshu Tripathi <subhanst@amdocs.com>
2024-01-26 14:00:59 +0530

    changes 2

--- src/main/java/com/example/productservicescaler/services/FakeStoreProductService.java
+++ src/main/java/com/example/productservicescaler/services/FakeStoreProductService.java
@@ -1,20 +1,9 @@
 package com.example.productservicescaler.services;
 
 import com.example.productservicescaler.clients.FakeStoreClient.FakeStoreClient;
-import com.example.productservicescaler.convertDTOs.FakeStoreDTOAndProduct;
 import com.example.productservicescaler.clients.FakeStoreClient.FakeStoreProductDTO;
 import com.example.productservicescaler.dtos.ProductDTO;
-import com.example.productservicescaler.exceptions.WrongIdException;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RequestCallback;
-import org.springframework.web.client.ResponseExtractor;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Optional;
